9d15f8cd75bfe001539b21e2a846d601f177b051
Anatoliy Popov <popov.anatoly@gmail.com>
2019-05-24 07:35:56 +0300

    test ac_site rename

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Hits-of-Code](https://hitsofcode.com/github/testpass1982/ac_lipetsk_site)](https://hitsofcode.com/view/github/testpass1982/ac_lipetsk_site_site)
+[![Hits-of-Code](https://hitsofcode.com/github/testpass1982/ac_site)](https://hitsofcode.com/view/github/testpass1982/ac_site_site)
 
-# ac_lipetsk_site
+# ac_site
--- ac_site/wsgi.py
+++ ac_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ac_lipetsk_site project.
+WSGI config for ac_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ac_lipetsk_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ac_site.settings')
 
 application = get_wsgi_application()
