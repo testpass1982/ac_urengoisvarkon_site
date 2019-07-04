d5429f7f20235ec905933d096cf6141e06457e67
Anatoliy Popov <popov.anatoly@gmail.com>
2019-07-04 10:11:08 +0300

    Fabric deploy: Thu Jul  4 10:11:08 2019

--- fabfile.py
+++ fabfile.py
@@ -482,10 +482,10 @@ def local_push():
     for line in output.splitlines():
         if any(['нечего коммитить' in line, 'nothing to commit' in line]):
             print('NOTHING TO COMMIT')
-            return
+            pass
         else:
             local('git add .')
-            local('git commit -m "Fabric deploy: {m}"'.format(m=time.ctime))
+            local('git commit -m "Fabric deploy: {m}"'.format(m=time.ctime()))
             local('git push -u origin master')
 
 def deploy():
