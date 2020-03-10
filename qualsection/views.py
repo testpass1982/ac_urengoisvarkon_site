445cc56790eb2cf8c0d7db6ce948fb6662948ee0
Anatoliy Popov <popov.anatoly@gmail.com>
2020-03-10 16:58:47 +0300

    cok page

--- qualsection/views.py
+++ qualsection/views.py
@@ -15,11 +15,18 @@ def main(request):
         })
 
     cok_exam_places = CokQualExamPlace.objects.all().order_by('number')
+    cok_zayavki = {
+        "zayavka_1": CokDocument.objects.filter(pseudo="zayavka_1").first(),
+        "zayavka_2": CokDocument.objects.filter(pseudo="zayavka_2").first()
+    }
+    pravila = CokDocument.objects.filter(pseudo="pravila").first()
     # import pdb; pdb.set_trace()
     content = {
         "cok_place": cok_place,
         "cok_contacts": cok_contacts,
         "profqualifications": profqualifications,
-        "cok_exam_places": cok_exam_places
+        "cok_exam_places": cok_exam_places,
+        "cok_zayavki": cok_zayavki,
+        "pravila": pravila
     }
     return render(request, 'qualsection/main_template.html', content)
